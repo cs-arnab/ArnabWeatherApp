dbdf6957c6a9aa1a09cdb9da41b6419718223719
cs-arnab <cs.arnabdana@gmail.com>
2021-05-28 13:30:50 +0530

    learn update

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.get("/weather",(req,res)=>{
 })
 app.get("*",(req,res)=>{
     res.render("404.hbs",{
-        errorMsg:'Oops! Page Not Found'
+        errorMsg:'Oops ! Page Not Found'
     })
 }) 
 
